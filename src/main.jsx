ade08a50b4f9f50b814b8f7916ac6e36f9342683
rokonroni <rokonuzzamanroni@gmail.com>
2023-11-08 19:37:14 +0600

    Tanstack query added when data isPending

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import AuthProvider from './providers/AuthProvider'
-import { HelmetProvider } from 'react-helmet-async'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Router'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
           <RouterProvider router={router} />
-      </HelmetProvider>
-    </AuthProvider>
-  </React.StrictMode>,
-)
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,42 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../../components/Banner/Banner";
 import AllTab from "../../components/AllTab/AllTab";
 import AllJobs from "../../components/AllJobs/AllJobs";
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 import CTA from "../../components/CTA/CTA";
 import WebDev from "../../components/WebDev/WebDev";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
-  const allJobs = useLoaderData();
+  // const allJobs = useLoaderData();
+
+  const {isPending, data: allJobs} = useQuery({
+    queryKey:["allJobs"],
+    queryFn: async () =>{
+      const res = await fetch("https://job-finder-server-tau.vercel.app/allJobs")
+      return res.json();
+    }
+
+  })
+
+
   const [searchText, setSearchText] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All Jobs");
   const handleSearchCategory = (searchText) => {
     setSearchText(searchText);
   };
 
- const filteredJobs = allJobs.filter((job) => {
+ const filteredJobs = allJobs?.filter((job) => {
   return job.jobCategory.toLowerCase().includes(searchText.toLowerCase()) || job.jobCategory.toLowerCase().includes(selectedCategory.toLowerCase())
 });
+if (isPending) {
+  return (
+            <>
+            <div className=" text-center h-[60vh] ">
+                <div className="mt-48 loading loading-infinity loading-lg"></div>
+            </div>
+            </>
+        )
+}
 
   return (
     <>
